f0cbf9ab55670c1497d23d95ab1a8f8dc52b15cb
Jorge Luis Pérez <jolisper@gmail.com>
2011-07-06 02:07:29 -0300

    Tidied up code

--- example/SimpleCalculator/bin/generate.rb
+++ example/SimpleCalculator/bin/generate.rb
@@ -16,11 +16,8 @@ FileUtils.mkdir( output_dir )
 # Working on output directory
 FileUtils.chdir( output_dir )
 
-puts File.expand_path("..#{sep}" + grammar_dir) + "#{sep}*.g"
-
 # Generate lexer and parser
 FileUtils.cp( Dir.glob( File.expand_path("..#{sep}" + grammar_dir) + "#{sep}*.g" ), "." )
-puts Dir.pwd
 system("java -classpath ..#{sep}lib#{sep}antlr-3.1.1.jar org.antlr.Tool " + Dir.glob('*.g').join(' '))
 
 # Remove all temp files
